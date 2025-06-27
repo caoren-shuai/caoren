051da5055fccd131254522dd15ff358c9d6315bd
xiahui010421 <xiahuibb@163.com>
2025-06-27 23:34:43 +0800

    cmake之后

---
+++ build/CMakeFiles/MySingleton.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MySingleton.dir/Singleton.cpp.o"
+  "CMakeFiles/MySingleton.dir/Singleton.cpp.o.d"
+  "CMakeFiles/MySingleton.dir/main.cpp.o"
+  "CMakeFiles/MySingleton.dir/main.cpp.o.d"
+  "MySingleton"
+  "MySingleton.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MySingleton.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/MySingleton.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for MySingleton.
+# This may be replaced when dependencies are built.
